4613918a26328208beea55d8115e6133d49c6261
Ashish <ashishnick.private@gmail.com>
2022-11-16 14:25:43 +0530

    disaplayed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,18 +11,43 @@ const Home = () => {
 
   useEffect(() => {
     dispatch(fetchPopularGames());
-  });
+  }, [dispatch]);
 
-  const games = useSelector((state) => state.games);
-
-  console.log(games);
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
 
   return (
-    <div>
-      Home
-      <Game />
-    </div>
+    <GameList>
+      <h2>Upcoming Games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <>
+            <Game
+              key={game.id}
+              name={game.name}
+              released={game.released}
+              id={game.id}
+              image={game.background_image}
+            />
+          </>
+        ))}
+      </Games>
+    </GameList>
   );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 3rem;
+`;
 
 export default Home;
